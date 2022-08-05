1050d32b79b279b8363d30e98cfb780e8f1c8ada
Enter <deletedenter@gmail.com>
2022-08-05 23:57:38 +0900

    Add:Directory Browser

--- EraCsvManager/MainWindow.xaml.cs
+++ EraCsvManager/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using EraCsvManager.VM;
 
 namespace EraCsvManager
 {
@@ -22,6 +23,17 @@ namespace EraCsvManager
     {
         public MainWindow()
         {
+        }
+        
+        public MainWindowVM main = new MainWindowVM();
+
+        private void SelectErbExeDir(object sender, RoutedEventArgs e)
+        {
+            txtRootDir.Text = main.SelectExeDir("root");
+        }
+        private void SelectOutDir(object sender, RoutedEventArgs e)
+        {
+            txtOutDir.Text = main.SelectExeDir("out");
         }
     }
 }
