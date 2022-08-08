6d000c82f16c380192b9e5e5c900c3890948d2b7
ZC <ironlizardster@gmail.com>
2022-08-08 19:45:30 -0400

    Updating checkbox enable conditoins

---
+++ EraCsvManager/Converter/FolderPathIsPresentConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Windows.Data;
+
+namespace EraCsvManager.Converter
+{
+    public class FolderPathIsPresentConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return Directory.Exists(value.ToString());
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
